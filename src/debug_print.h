1297ea9ab066966455920468c89dc9f7f39c7ccd
Jozef Sovcik <jozef@sovcik.net>
2019-11-03 22:51:26 +0100

    extending debug_print

--- src/debug_print.h
+++ src/debug_print.h
@@ -1,15 +1,50 @@
-#ifndef NODEBUG_PRINT
-#ifdef DEBUG_ESP_PORT
-#define DEBUG_PRINT(...) DEBUG_ESP_PORT.printf( __VA_ARGS__ )
-#define DEBUG_ARRAY(ARR,ARR_L) for (uint16_t _aidx =0; _aidx<ARR_L;_aidx++) {DEBUG_ESP_PORT.printf("%02X ",*(ARR+_aidx)); if (_aidx%20 == 19)DEBUG_ESP_PORT.printf("\n");}
-#else
-#define DEBUG_PRINT(...) os_printf( __VA_ARGS__ )
-#define DEBUG_ARRAY(ARR,ARR_L) for (uint16_t _aidx =0; _aidx<ARR_L;_aidx++) {os_printf("%02X ",*(ARR+_aidx)); if (_aidx%20 == 19)os_printf("\n");}
-#endif
+#ifndef __DEBUG_PRINT_H__
+
+#if !defined(NODEBUG_PRINT) && defined(DEBUG_PORT)
+
+#define DEBUG_PRINT(...) DEBUG_PORT.printf( __VA_ARGS__ )
+#define DEBUG_ARRAY(ARR,ARR_L) for (uint16_t _aidx =0; _aidx<ARR_L;_aidx++) {DEBUG_PORT.printf("%02X ",*(ARR+_aidx)); if (_aidx%20 == 19)DEBUG_PORT.printf("\n");}
+#define DEBUG_ARRAY_F(ARR,ARR_L,ARR_F) for (uint16_t _aidx =0; _aidx<ARR_L;_aidx++) {DEBUG_PORT.printf(ARR_F,*(ARR+_aidx)); if (_aidx%20 == 19)DEBUG_PORT.printf("\n");}
+
+#if defined(PGMSPACE_INCLUDE) || defined(__PGMSPACE_H_)
+#define DEBUG_PRINT_PGM(...) DEBUG_PORT.printf_P( __VA_ARGS__ )
+#else 
+#define DEBUG_PRINT_PGM(...)
 #endif
 
-#ifndef DEBUG_PRINT
+#else
 #define DEBUG_PRINT(...)
+#define DEBUG_PRINT_PGM(...)
 #define DEBUG_ARRAY(...)
+#define DEBUG_ARRAY_F(...)
+
+#endif
+
+//--
+
+#if !defined(NODEBUG_PRINT) && defined(CONSOLE_PORT)
+
+#define CONSOLE(...) CONSOLE_PORT.printf( __VA_ARGS__ )
+#define CONSOLE_ARRAY(ARR,ARR_L) for (uint16_t _aidx =0; _aidx<ARR_L;_aidx++) {CONSOLE_PORT.printf("%02X ",*(ARR+_aidx)); if (_aidx%20 == 19)CONSOLE_PORT.printf("\n");}
+#define CONSOLE_ARRAY_F(ARR,ARR_L,ARR_F) for (uint16_t _aidx =0; _aidx<ARR_L;_aidx++) {CONSOLE_PORT.printf(ARR_F,*(ARR+_aidx)); if (_aidx%20 == 19)CONSOLE_PORT.printf("\n");}
+
+#if defined(PGMSPACE_INCLUDE) || defined(__PGMSPACE_H_)
+#define CONSOLE_PGM(...) CONSOLE_PORT.printf_P( __VA_ARGS__ )
+#else
+#define CONSOLE_PGM(...)
+#endif
+
+#else
+
+#define CONSOLE(...)
+#define CONSOLE_ARRAY(...)
+#define CONSOLE_ARRAY_F(...)
+#define CONSOLE_PGM
+
+#endif
+
+#ifndef NODEBUG_PRINT
 #define NODEBUG_PRINT
+#endif
+
 #endif
